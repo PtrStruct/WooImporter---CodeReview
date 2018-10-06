a4fe3b4fbb9465dc5b5a91622e70a1a508491b1a
Varga <silentwolfdev@outlook.com>
2018-10-06 02:03:33 +0200

    Add project files.

---
+++ WooImporter/MVVM/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WooImporter.MVVM.ViewModels;
+
+namespace WooImporter.MVVM
+{
+    public class BaseViewModel
+    {
+        public ProductsViewModel ProductsViewModel { get; } = new ProductsViewModel();
+    }
+}
---
+++ WooImporter/MVVM/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WooImporter.Annotations;
+using WooImporter.MVVM.Models;
+
+namespace WooImporter.MVVM.ViewModels
+{
+    public class ProductsViewModel : INotifyPropertyChanged
+    {
+
+        private ObservableCollection<ProductModel> _products = new ObservableCollection<ProductModel>();
+
+        public ObservableCollection<ProductModel> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                OnPropertyChanged();
+            }
+        }
+
+        
+        public ProductsViewModel()
+        {
+            _products.Add(new ProductModel {Name = "Item 1", Price = "This should be a double"});
+            _products.Add(new ProductModel {Name = "Item 2", Price = "This should be a double"});
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
